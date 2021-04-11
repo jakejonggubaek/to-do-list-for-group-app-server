0fe03755819580e9e3cb83eae628c2832ff2123b
jakejonggubaek <jakejonggubaek@gmail.com>
2021-04-10 23:26:41 -0400

    added heroku config var

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const RoomModel = require('./models/ToDo');
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://jakejonggubaek:Cdspace1!@cluster0.09veq.mongodb.net/toDo?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
     }
